b044d039e79c4fc6e11e0ef5199bd40f8820e7cc
jamesnromach <jamesR@magenic.com>
2017-04-26 15:54:45 -0500

    3929 Create base test template

---
+++ Extensions/VisualStudioQatExtension/ProjectTemplates/Magenic Test/NUnit/QATNUnitBaseTemplate/Tests/BaseGenericTest.cs
@@ -0,0 +1,22 @@
+﻿using Magenic.MaqsFramework.BaseTest;
+using NUnit.Framework;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// $safeitemname$ test class
+    /// </summary>
+    [TestFixture]
+    public class $safeitemname$ : BaseTest
+    {
+        /// <summary>
+        /// Sample test
+        /// </summary>
+        [Test]
+        public void SampleTest()
+        {
+            this.Log.LogMessage("Is true True?");
+            Assert.True(true, "true Is Not True");
+        }
+    }
+}
