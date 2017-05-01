ef1ddfb3e9d58433d05f54435138093dc56175bd
jamesnromach <jamesR@magenic.com>
2017-05-01 10:17:05 -0500

    Added base to selenium

---
+++ Extensions/VisualStudioQatExtension/Test/ItemTemplates/Magenic Test/NUnit/QATNUnitEmailTest/NUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Magenic.MaqsFramework.BaseEmailTest;
+using NUnit.Framework;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// $safeitemname$ test class
+    /// </summary>
+    [TestFixture]
+    public class $safeitemname$ : BaseEmailTest
+    {
+        /// <summary>
+        /// Sample test
+        /// </summary>
+        [Test]
+        public void SampleTest()
+        {
+            // TODO: Add test code
+            // Assert.IsTrue(this.EmailWrapper.CanAccessEmailAccount(), "Could not access account");
+        }
+    }
+}
---
+++ Extensions/VisualStudioQatExtension/Test/ProjectTemplates/Magenic Test/NUnit/QATNUnitWebServiceTemplate/Tests/WebServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Magenic.MaqsFramework.BaseWebServiceTest;
+using NUnit.Framework;
+using WebServiceModel;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// Simple web service test class
+    /// </summary>
+    [TestFixture]
+    public class $safeitemname$ : BaseWebServiceTest
+    {
+        /// <summary>
+        /// Get single product as XML
+        /// </summary>
+        [Test]
+        public void GetXmlDeserialized()
+        {
+            ProductXml result = this.WebServiceWrapper.Get<ProductXml>("/api/XML_JSON/GetProduct/1", "application/xml", false);
+
+            Assert.AreEqual(result.Id, 1, "Expected to get product 1");
+        }
+
+        /// <summary>
+        /// Get single product as Json
+        /// </summary>
+        [Test]
+        public void GetJsonDeserialized()
+        {
+            ProductJson result = this.WebServiceWrapper.Get<ProductJson>("/api/XML_JSON/GetProduct/1", "application/json", false);
+
+            Assert.AreEqual(result.Id, 1, "Expected to get product 1");
+        }
+    }
+}
