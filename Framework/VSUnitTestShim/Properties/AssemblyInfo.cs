bbb6aef9de3c4f3bacb728ea3e3fc03663165be8
Troy Walsh <TroyW@magenic.com>
2017-07-27 16:42:23 -0500

    Update to V 4.0.2

--- Framework/VSUnitTestShim/Properties/AssemblyInfo.cs
+++ Framework/VSUnitTestShim/Properties/AssemblyInfo.cs
@@ -10,13 +10,13 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("4.0.1")]
+[assembly: AssemblyVersion("4.0.2")]
 [assembly: CLSCompliant(true)]
 [assembly: AssemblyCompany("FAKE")]
 [assembly: AssemblyCopyright("© FAKE")]
 [assembly: AssemblyDefaultAlias("Microsoft.VisualStudio.QualityTools.UnitTestFramework.dll")]
 [assembly: AssemblyDescription("Microsoft.VisualStudio.QualityTools.UnitTestFramework.dll")]
-[assembly: AssemblyFileVersion("4.0.1")]
+[assembly: AssemblyFileVersion("4.0.2")]
 [assembly: AssemblyInformationalVersion("12.0.21005.1")]
 [assembly: AssemblyProduct("FAKE")]
 [assembly: AssemblyTitle("Microsoft.VisualStudio.QualityTools.UnitTestFramework.dll")]
