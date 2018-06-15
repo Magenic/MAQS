614f25d2e279310e9f849b1420d90fd19b4631f5
Troy Walsh <TroyW@magenic.com>
2018-06-15 15:37:15 -0500

    Add Winappdriver support

---
+++ Framework/AppiumUnitTests/AppiumWinAppUnitTests.cs
@@ -0,0 +1,47 @@
+﻿//--------------------------------------------------
+// <copyright file="AppiumWinAppUnitTests.cs" company="Magenic">
+//  Copyright 2018 Magenic, All rights Reserved
+// </copyright>
+// <summary>Test class for windows application driver related functions</summary>
+//--------------------------------------------------
+using Magenic.MaqsFramework.BaseAppiumTest;
+using Magenic.MaqsFramework.Utilities.Helper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace AppiumUnitTests
+{
+    /// <summary>
+    /// Windows application driver related Appium tests
+    /// </summary>
+    [TestClass]
+    public class AppiumWinAppUnitTests : BaseAppiumTest
+    {
+        /// <summary>
+        /// Tests the creation of the Appium Windows application driver
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestCategories.Appium)]
+        public void AppiumIOSDriverTest()
+        {
+            Assert.IsNotNull(this.TestObject.AppiumDriver);
+        }
+
+        /// <summary>
+        /// Sets capabilities for testing the Windows application driver creation
+        /// </summary>
+        /// <returns>Windows application driver instance of the Appium Driver</returns>
+        protected override AppiumDriver<IWebElement> GetMobileDevice()
+        {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.SetCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            capabilities.SetCapability(MobileCapabilityType.Udid, "0C0E26E7-966B-4C89-A765-32C5C997A456");
+            return new WindowsDriver<IWebElement>(new Uri("http://127.0.0.1:4723"), capabilities);
+        }
+    }
+}
