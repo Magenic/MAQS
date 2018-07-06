b3492237d0a71d6e4b180f642b46b65319ec98fd
Troy Walsh <TroyW@magenic.com>
2018-07-06 15:51:26 -0500

    Add new template files

---
+++ Extensions/VisualStudioQatExtension/ProjectTemplates/Magenic Test Core/QATBaseTemplate/Tests/BaseGenericTestVSUnit.cs
@@ -0,0 +1,22 @@
+﻿using Magenic.Maqs.BaseTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// BaseGenericTest test class
+    /// </summary>
+    [TestClass]
+    public class BaseGenericTest : BaseTest
+    {
+        /// <summary>
+        /// Sample test
+        /// </summary>
+        [TestMethod]
+        public void SampleTest()
+        {
+            this.TestObject.Log.LogMessage("Start Test");
+            Assert.IsTrue(true, "True is Not True");
+        }
+    }
+}
