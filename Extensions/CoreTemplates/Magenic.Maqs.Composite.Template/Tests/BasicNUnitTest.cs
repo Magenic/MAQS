b3e10d3c700f5dccb51302a058ba7c60468bd0ec
jeffz <jeffz@magenic.com>
2018-08-09 16:47:32 -0500

    Create nuget .net Core templates

---
+++ Extensions/CoreTemplates/Magenic.Maqs.Composite.Template/Tests/BasicNUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Magenic.Maqs.BaseTest;
+using NUnit.Framework;
+
+namespace Tests
+{
+    /// <summary>
+    /// BasicNUnitTest test class with NUnit
+    /// </summary>
+    [TestFixture]
+    public class BasicNUnitTest : BaseTest
+    {
+        /// <summary>
+        /// Sample test
+        /// </summary>
+        [Test]
+        public void SampleTest()
+        {
+            this.TestObject.Log.LogMessage("Start Test");
+            Assert.IsTrue(true, "True is Not True");
+        }
+    }
+}
---
+++ Extensions/CoreTemplates/Magenic.Maqs.Email.Template/Tests/EmailTestVSUnit.cs
@@ -0,0 +1,22 @@
+﻿using Magenic.Maqs.BaseEmailTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    /// <summary>
+    /// Tests test class
+    /// </summary>
+    [TestClass]
+    public class EmailTestVSUnit : BaseEmailTest
+    {
+        /// <summary>
+        /// Sample test
+        /// </summary>
+        [TestMethod]
+        public void SampleTest()
+        {
+            // TODO: Add test code
+            // Assert.IsTrue(this.EmailDriver.CanAccessEmailAccount(), "Could not access account");
+        }
+    }
+}
