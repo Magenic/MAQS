a92e3b6123086f2a963e0ecd19fab5cefbd07809
Robert Wroblewski <RobertW@magenic.com>
2019-06-05 10:51:58 -0500

    updating file logger and html logger LogMessage method to be thread safe in the context of it's own test.
    Added a few unit tests to verify the change.

--- Framework/Utilities/Logging/FileLogger.cs
+++ Framework/Utilities/Logging/FileLogger.cs
@@ -104,25 +104,17 @@ namespace Magenic.Maqs.Utilities.Logging
             if (this.ShouldMessageBeLogged(messageType))
             {
                 // Log the message
-                try
-                {
-                    StreamWriter writer = new StreamWriter(this.FilePath, true);
+                lock (this.fileLock)
+                { 
+                    using (StreamWriter writer = new StreamWriter(this.FilePath, true))
+                    {
 
-                    string date = DateTime.UtcNow.ToString(Logger.DEFAULTDATEFORMAT, CultureInfo.InvariantCulture);
-                    writer.WriteLine(StringProcessor.SafeFormatter("{0}{1}", Environment.NewLine, date));
-                    writer.Write(StringProcessor.SafeFormatter("{0}:\t", messageType.ToString()));
+                        string date = DateTime.UtcNow.ToString(Logger.DEFAULTDATEFORMAT, CultureInfo.InvariantCulture);
+                        writer.WriteLine(StringProcessor.SafeFormatter("{0}{1}", Environment.NewLine, date));
+                        writer.Write(StringProcessor.SafeFormatter("{0}:\t", messageType.ToString()));
 
-                    writer.WriteLine(StringProcessor.SafeFormatter(message, args));
-
-                    writer.Flush();
-                    writer.Close();
-                }
-                catch (Exception e)
-                {
-                    // Failed to write to the event log, write error to the console instead
-                    ConsoleLogger console = new ConsoleLogger();
-                    console.LogMessage(MessageType.ERROR, StringProcessor.SafeFormatter("Failed to write to event log because: {0}", e.Message));
-                    console.LogMessage(messageType, message, args);
+                        writer.WriteLine(StringProcessor.SafeFormatter(message, args));
+                    }
                 }
             }
         }
---
+++ Framework/UtilitiesUnitTests/FileLoggerThreadSafetyTests.cs
@@ -0,0 +1,65 @@
+﻿using Magenic.Maqs.Utilities.Helper;
+using Magenic.Maqs.Utilities.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UtilityLogger = Magenic.Maqs.Utilities.Logging.Logger;
+
+namespace UtilitiesUnitTests
+{
+
+    [TestClass]
+    public class FileLoggerThreadSafetyTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [TestCategory(TestCategories.Utilities)]
+        public void FileLoggerMultipleTaskTest()
+        {
+            var fileLogger = new FileLogger(logFolder: "Log", name: $"{this.TestContext.TestName}.txt");
+
+            this.ExecuteLoggingTasks(fileLogger);
+        }
+
+        [TestMethod]
+        [TestCategory(TestCategories.Utilities)]
+        public void HtmlFileLoggerMultipleTaskTest()
+        {
+            var fileLogger = new HtmlFileLogger(logFolder: "Log", name: $"{this.TestContext.TestName}.html");
+
+            this.ExecuteLoggingTasks(fileLogger);
+        }
+
+        /// <summary>
+        /// logs a message using 5 tasks to verify the log file is locked when 
+        /// being written to
+        /// </summary>
+        /// <param name="logger"> the logger that is writing to the file</param>
+        private void ExecuteLoggingTasks(UtilityLogger logger)
+        {
+            var loggingTasks = new List<Task>();
+
+            loggingTasks.Add(Task.Run(() =>
+            {
+                logger.LogMessage("parallel task 1");
+
+                // halting thread to ensure tasks enter wait state
+                Thread.Sleep(5000);
+
+            }));
+
+            loggingTasks.Add(Task.Run(() => { logger.LogMessage("parallel task 2"); }));
+            loggingTasks.Add(Task.Run(() => { logger.LogMessage("parallel task 3"); }));
+            loggingTasks.Add(Task.Run(() => { logger.LogMessage("parallel task 4"); }));
+            loggingTasks.Add(Task.Run(() => { logger.LogMessage("parallel task 5"); }));
+
+            Task.WaitAll(loggingTasks.ToArray());
+        }
+    }
+}
