b90168fa3f0e0b00f87e05e807010cbf87e728fe
Jake Ferm <JakeFe@magenic.com>
2020-04-13 14:28:27 -0500

    Adding screenshot tests into Framework

--- Framework/FrameworkUnitTests/Selenium.cs
+++ Framework/FrameworkUnitTests/Selenium.cs
@@ -5,9 +5,12 @@
 // <summary>Core Selenium unit tests</summary>
 //--------------------------------------------------
 using Magenic.Maqs.BaseSeleniumTest;
+using Magenic.Maqs.BaseSeleniumTest.Extensions;
 using Magenic.Maqs.Utilities.Helper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 
 namespace FrameworkUnitTests
 {
@@ -20,12 +23,92 @@ namespace FrameworkUnitTests
     public class Selenium : BaseSeleniumTest
     {
         /// <summary>
+        /// Unit testing site URL - Login page
+        /// </summary>
+        private static readonly string TestSiteUrl = SeleniumConfig.GetWebSiteBase();
+
+        /// <summary>
         /// Run a very simple selenium test
         /// </summary>
         [TestMethod]
         public void CanRunSeleniumTest()
         {
             Assert.IsNotNull(this.TestObject.WebDriver);
+        }
+
+        /// <summary>
+        /// Verify that CaptureScreenshot captured is in the bitmap image format
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestCategories.Selenium)]
+        public void CaptureScreenshotBmpFormat()
+        {
+            WebDriver.Navigate().GoToUrl(TestSiteUrl);
+            WebDriver.Wait().ForPageLoad();
+            string screenShotPath = SeleniumUtilities.CaptureScreenshot(WebDriver, TestObject, "TempTestDirectory", "TempTestFilePath", ScreenshotImageFormat.Bmp);
+            Assert.IsTrue(File.Exists(screenShotPath), "Fail to find screenshot");
+            Assert.AreEqual(".Bmp", Path.GetExtension(screenShotPath), "The screenshot format was not in '.Bmp' format");
+            File.Delete(screenShotPath);
+        }
+
+        /// <summary>
+        /// Verify that CaptureScreenshot captured is in the Graphics Interchange Format
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestCategories.Selenium)]
+        public void CaptureScreenshotGifFormat()
+        {
+            WebDriver.Navigate().GoToUrl(TestSiteUrl);
+            WebDriver.Wait().ForPageLoad();
+            string screenShotPath = SeleniumUtilities.CaptureScreenshot(WebDriver, TestObject, "TempTestDirectory", "TempTestFilePath", ScreenshotImageFormat.Gif);
+            Assert.IsTrue(File.Exists(screenShotPath), "Fail to find screenshot");
+            Assert.AreEqual(".Gif", Path.GetExtension(screenShotPath), "The screenshot format was not in '.Gif' format");
+            File.Delete(screenShotPath);
+        }
+
+        /// <summary>
+        /// Verify that CaptureScreenshot captured is in the Joint Photographic Experts Group format
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestCategories.Selenium)]
+        public void CaptureScreenshotJpegFormat()
+        {
+            WebDriver.Navigate().GoToUrl(TestSiteUrl);
+            WebDriver.Wait().ForPageLoad();
+            string screenShotPath = SeleniumUtilities.CaptureScreenshot(WebDriver, TestObject, "TempTestDirectory", "TempTestFilePath", imageFormat: ScreenshotImageFormat.Jpeg);
+            Assert.IsTrue(File.Exists(screenShotPath), "Fail to find screenshot");
+            Assert.AreEqual(".Jpeg", Path.GetExtension(screenShotPath), "The screenshot format was not in '.Jpeg' format");
+            File.Delete(screenShotPath);
+        }
+
+        /// <summary>
+        /// Verify that CaptureScreenshot captured is in the Portable Network Graphics format
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestCategories.Selenium)]
+        public void CaptureScreenshotPngFormat()
+        {
+            WebDriver.Navigate().GoToUrl(TestSiteUrl);
+            WebDriver.Wait().ForPageLoad();
+            string screenShotPath = SeleniumUtilities.CaptureScreenshot(WebDriver, TestObject, "TempTestDirectory", "TempTestFilePath", imageFormat: ScreenshotImageFormat.Png);
+            Assert.IsTrue(File.Exists(screenShotPath), "Fail to find screenshot");
+            Assert.AreEqual(".Png", Path.GetExtension(screenShotPath), "The screenshot format was not in '.Png' format");
+            File.Delete(screenShotPath);
+        }
+
+        /// <summary>
+        /// Verify that CaptureScreenshot captured is in the Tagged Image File Format
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestCategories.Selenium)]
+        public void CaptureScreenshotTiffFormat()
+        {
+            WebDriver.Navigate().GoToUrl(TestSiteUrl);
+            WebDriver.Wait().ForPageLoad();
+            string screenShotPath = SeleniumUtilities.CaptureScreenshot(WebDriver, TestObject, "TempTestDirectory", "TempTestFilePath", ScreenshotImageFormat.Tiff);
+            Assert.IsTrue(File.Exists(screenShotPath), "Fail to find screenshot");
+            Assert.AreEqual(".Tiff", Path.GetExtension(screenShotPath), "The screenshot format was not in '.Tiff' format");
+            File.Delete(screenShotPath);
         }
     }
 }
