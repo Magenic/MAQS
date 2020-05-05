24ba77e760e875a4032788b440e5b758e6be7f38
Brannen Hall <BrannenH@magenic.com>
2020-05-05 08:55:30 -0500

    Add long polling

--- Docker/SqlServer/initialize_and_start_sqlserver.sh
+++ Docker/SqlServer/initialize_and_start_sqlserver.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 { 
-    # Wait 30 seconds for SQL server to start up.
-    # Ideally, SQL server would have a hook to run scripts once the database is ready.
-    sleep 30s
+    # Wait for SQL Server to start up
+    # Check if the server is ready
+    not_ready=1
+    while [ $not_ready != 0 ]
+    do
+        # Wait for the return code of the following statement to be zero
+        /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P magenicMAQS2 -d master -Q "SELECT TOP 1 message_id FROM sys.messages"
+        not_ready=$?
+
+        if [ $not_ready != 0 ]
+        then
+            echo "Could not contact sql server, will try again in 5 seconds."
+            sleep 5s
+        fi
+    done
+
     echo "Started initializing database"
     # Set up the schema and stored procedures
     /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P magenicMAQS2 -d master -i `dirname $0`/schema.sql
