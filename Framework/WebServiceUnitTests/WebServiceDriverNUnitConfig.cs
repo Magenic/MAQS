b20bd24daff4e76b3d1a018c48a1d62f48933bd7
Troy Walsh <TroyW@magenic.com>
2021-08-18 15:38:40 -0500

    Fix test

--- Framework/WebServiceUnitTests/WebServiceDriverNUnitConfig.cs
+++ Framework/WebServiceUnitTests/WebServiceDriverNUnitConfig.cs
@@ -27,8 +27,8 @@ namespace WebServiceTesterUnitTesting
         {
             // Set overrides
             MethodInfo dynMethod = NUnit.Framework.TestContext.Parameters.GetType().GetMethod("Add", BindingFlags.NonPublic | BindingFlags.Instance);
-            dynMethod.Invoke(NUnit.Framework.TestContext.Parameters, new object[] { "OverrideNUnitTest", "Value" });
-            dynMethod.Invoke(NUnit.Framework.TestContext.Parameters, new object[] { "OverrideNUnitTestNew", "Value2" });
+            dynMethod.Invoke(NUnit.Framework.TestContext.Parameters, new object[] { "MagenicMaqs:OverrideNUnitTest", "Value" });
+            dynMethod.Invoke(NUnit.Framework.TestContext.Parameters, new object[] { "magenicmaqs:OverrideNUnitTestNew", "Value2" });
 
             Config.UpdateWithNUnitTestParameters(NUnit.Framework.TestContext.Parameters);
         }
